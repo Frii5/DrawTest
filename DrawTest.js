b4cb32241cd5a573eab83aff1f8d8bf1828d54b6
Frii5 <Kristian.friis@live.dk>
2017-01-16 13:36:05 +0100

    DrawApp 2.0
    it draws

--- DrawTest.js
+++ DrawTest.js
@@ -1,55 +1,109 @@
-var ctx
-var lastPt=null;
-var canvas
+var ctx; //context
+var lastPt = null; //last point
+var touchzone;
+var offsetleft = 0; //offset if parent is present
+var offsettop = 0; // --||--
+var count = 1;
 
-
+//init/initialize starts when body loads
 function init() {
-    var touchzone = document.getElementById("mycanvas");
-    touchzone.addEventListener("touchmove", draw, false)
-    touchzone.addEventListener("touchend", end, false);
-    ctx = touchzone.getContext("2d");
+    
+    touchzone = document.getElementById("mycanvas"); //define variable "touchzone"
+    touchzone.addEventListener("touchmove", draw, false); //Adding event "touchmove", to function "draw"
+    touchzone.addEventListener("touchend", end, false); //Adding event "touchend", to function "end"
+    ctx = touchzone.getContext("2d"); //defining ctx variable, as 2d plane.
 }
 
-function draw(e) {
-    e.preventDefault();
+function draw(e) { 
+    e.preventDefault(); //cancels ongoing event
     
-    if(lastPt!=null){ 
+    if (lastPt != null) { //if lastPt is anything BUT null, beginPath...
         
-        ctx.beginPath();
-        ctx.lineTo(lastPt.x, lastPt.y);
-        ctx.lineTo(e.touches[0].pageX, e.touches[0].pageY);
+        ctx.beginPath(); //start drawing
+        ctx.lineTo(lastPt.x, lastPt.y); //draw a line starting from (x,y)
+        ctx.lineTo(e.touches[0].pageX - offsetleft, e.touches[0].pageY - offsettop);
         ctx.stroke();
 
     }
-        lastPt = {x:e.touches[0].pageX, y:e.touches[0].pageY};
+        lastPt = {x: e.touches[0].pageX - offsetleft, y: e.touches[0].pageY - offsettop};
 }
- 
+
 function end(e) {
-    e.preventDefault
-    lastPt=null;
+    e.preventDefault();
+    lastPt = null;
 }
 
-function green() {
-    ctx.strokeStyle="green";
-}
-
-function blue() {
-    ctx.strokeStyle="blue";
-}
-
-function clear() {
-    ctx.strokeStyle="red";
-  
-}
-
-function red() {
-    ctx.strokeStyle="white";
-    ctx.clearRect(0, 0, 500, 500);
+function countfunc() {
+    
+    if (count == 1)
+    count = 2;
+    else if (count == 2)
+    count = 1;
 }
 
 function black() {
-    ctx.strokeStyle="black";
+    
+    if (count == 1) {
+    ctx.strokeStyle = "black";    
+    }
+    
+    else {
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, touchzone.width, touchzone.height);  
+    }
 }
+
+function green() {
+
+    if (count == 1) {
+    ctx.strokeStyle = "green";    
+    }
+    else {
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, touchzone.width, touchzone.height);
+    }
+}
+
+function blue() {
+    
+    if (count == 1) {
+    ctx.strokeStyle = "blue";    
+    }
+    else {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, touchzone.width, touchzone.height);
+    }
+  
+}
+
+function rød() {
+    
+    if (count == 1) {
+    ctx.strokeStyle = "red";    
+    }
+    else {
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, touchzone.width, touchzone.height);
+    }
+  
+}
+
+function clr() {
+    
+    ctx.strokeStyle = "rgba(255, 255, 255, 0.01)";
+    ctx.clearRect(0, 0, touchzone.width, touchzone.height);
+    navigator.vibrate([500,200,500]);
+   
+}
+
+
+
+
+
+
+
+
+
 
 
 
